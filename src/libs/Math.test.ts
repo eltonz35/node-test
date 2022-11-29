ec08c59998bf048841712a0932ee63a56ace9aea
Elthon <eltonsilva.mecatronic@gmail.com>
2022-11-28 21:49:38 -0300

    Fazendo alteraçoes

--- src/libs/Math.test.ts
+++ src/libs/Math.test.ts
@@ -1,21 +1,23 @@
 import { Math } from './Math';
 
-it('should sum two numbers correctly', () => {
-    const response = Math.sum(5, 10);
-    expect(response).toBe(15);
-});
+describe('testing Math libary', () => {
+    it('should sum two numbers correctly', () => {
+        const response = Math.sum(5, 10);
+        expect(response).toBe(15);
+    });
 
-it('should subtract two numbers correctly', () => {
-    const response = Math.sub(4, 2);
-    expect(response).toBe(2);
-});
+    it('should subtract two numbers correctly', () => {
+        const response = Math.sub(4, 2);
+        expect(response).toBe(2);
+    });
 
-it('should multiply two numbers correctly', () => {
-    const response = Math.mut(3, 5);
-    expect(response).toBe(15)
-});
+    it('should multiply two numbers correctly', () => {
+        const response = Math.mut(2, 2);
+        expect(response).toBe(4);
+    });
 
-it('shold divide two numbers correctly', () => {
-    const response = Math.div(8, 2);
-    expect(response).toBe(4);
+    it('shold divide two numbers correctly', () => {
+        const response = Math.div(8, 2);
+        expect(response).toBe(4);
+    });
 });
